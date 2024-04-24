0f58cb15f6e58a99ac7dc480ad659b9a73d8c49a
Asus <mkmani176@gmail.com>
2024-04-24 11:03:21 +0530

    24/04/2024

---
+++ learn-app/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img className="app-logo" src="https://tse3.mm.bing.net/th?id=OIP.wJr2He8iXMNL3MQX4m1bawHaEK&pid=Api&P=0&h=180" alt="test" ></img>
+        <h1>Learn react js</h1>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
